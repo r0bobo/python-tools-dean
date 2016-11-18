4dfe13cef318c8a91983500f8807b48cf286c176
Dean Todevski <dean.todevski@gmail.com>
2016-11-18 23:41:48 +0100

    Updated code

--- utilities/log_activity.py
+++ utilities/log_activity.py
@@ -6,23 +6,42 @@ import datetime
 
 def main():
     log = Log()
-    log.log_successful('ssh_logger')
+    log.successful('ssh_logger')
+    log.failed('dl_yt_playlist')
 
 
 class Log:
     """."""
 
     config_reader = []
-    log_path = []
+    log_file = []
 
-    def __init__(self, log_path=None):
+    def __init__(self, log_file=None):
         """."""
         self.config_reader = conf_reader.ConfigReader()
-        self.log_path = self.config_reader.get('activity_log')
 
-    def log_successful(self, script_name):
-        message = 'Successfully finshed: %s' % script_name
-        print(message)
+        if log_file:
+            self.log_file = log_file
+        else:
+            self.log_file = self.config_reader.get('activity_log')
+
+    def write_log(self, script_name, successful):
+        if successful:
+            status = 'successfully finished'
+        else:
+            status = 'failed'
+
+        message = '%s    %s: %s\n' % (datetime.datetime.now().strftime('%Y %b %d %H:%M:%S'), script_name, status)
+
+        with open(self.log_file, 'a+') as f:
+            f.write(message)
+
+    def successful(self, script_name):
+        self.write_log(script_name, True)
+
+    def failed(self, script_name):
+        self.write_log(script_name, False)
+
 
 if __name__ == '__main__':
     main()
