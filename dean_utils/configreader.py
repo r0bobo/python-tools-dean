39b642a2dfaf15c154cf828c2330e20dfbd13db3
Dean Todevski <dean.todevski@gmail.com>
2017-01-22 01:26:58 +0100

    Update default paths

--- dean_utils/configreader.py
+++ dean_utils/configreader.py
@@ -21,16 +21,25 @@ class ConfigReader(configparser.ConfigParser):
 
     def readconf(self, filename=None):
         """."""
+        homedir = os.path.expanduser('~')
+
         default_config = collections.OrderedDict()
         default_config['GENERAL'] = {
             '# general settings': None
             }
         default_config['DL-YT-PLAYLIST'] = {
-            'youtube_playlist': 'https://www.youtube.com/playlist?list=PL1qRR_Q0qopRh_CE3FvXSFOHohDAYH_GN',
+            'youtube_playlist': 'https://www.youtube.com/playlist?list=PL1qRR_Q0qopRTcoibkO5Ar7wh0iiTewys',
+            # 'youtube_playlist': 'https://www.youtube.com/playlist?list=PL1qRR_Q0qopRh_CE3FvXSFOHohDAYH_GN',
             'download_location': '{:s}'.format(
-                os.path.join(os.path.expanduser('~'), 'Downloads', 'Youtube_Videos')),
+                os.path.join(homedir, 'Downloads', 'Youtube_Videos')),
             'download_log': '{:s}'.format(
-                os.path.join(os.path.expanduser('~'), 'youtube_dl.log'))
+                os.path.join(homedir, '.local', 'share',
+                             'dean', 'youtube_dl.log')
+                             ),
+            'download_archive': '{:s}'.format(
+                os.path.join(homedir, '.local', 'share',
+                             'dean', '.download_archive')
+                             ),
             }
         default_config['SSH-LOGGER'] = {
             'auth_log': '/var/log/auth.log'
@@ -38,7 +47,7 @@ class ConfigReader(configparser.ConfigParser):
 
         if not filename:
             config_file = os.path.join(
-                os.path.expanduser('~'), '.config', 'dean', 'dean.ini'
+                homedir, '.config', 'dean', 'dean.ini'
                 )
             self.logger.debug(
                 'No config file path specificed, using default: {:s}'
@@ -48,11 +57,13 @@ class ConfigReader(configparser.ConfigParser):
             config_file = os.path.expanduser(filename)
 
         try:
-            with open(config_file) as fp:
+            with open(config_file, 'r') as fp:
                 super().read_file(fp)
             self.logger.debug('Finished reading {:s}'.format(config_file))
         except FileNotFoundError:
+            self.logger.debug('Config file missing {:s}'.format(config_file))
             super().read_dict(default_config)
             os.makedirs(os.path.dirname(config_file), exist_ok=True)
-            with open(config_file, 'w') as fp:
+            with open(config_file, 'w+') as fp:
                 super().write(fp)
+                self.logger.info('Writing default config file to {:s}'.format(config_file))
